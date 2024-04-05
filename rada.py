402054dc6a9a0013a0e8767f0ccaab2f38daa878
radaservices <soporte@radaservices.cl>
2024-04-05 18:04:02 +0000

    este es el archivo1 de python

--- rada.py
+++ rada.py
@@ -3,3 +3,4 @@ lastName = input("Cual es tu apellido? ")
 location = input("Cual es tu localización? ")
 age = input("Cual es tu edad?")
 print("Hola " + firstName, lastName + "! Tu ubicación es "+ location +" y tienes "+ age +" años.")
+# este es mi primer archivo en python
