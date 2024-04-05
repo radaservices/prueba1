b21079d4266a6b5d72e47278833fb5ffdc15e822
radaservices <soporte@radaservices.cl>
2024-04-05 17:53:32 +0000

    este es el archivo1 de python

---
+++ rada.py
@@ -0,0 +1,5 @@
+firstName = input("Cual es tu nombre? ")
+lastName = input("Cual es tu apellido? ")
+location = input("Cual es tu localización? ")
+age = input("Cual es tu edad?")
+print("Hola " + firstName, lastName + "! Tu ubicación es "+ location +" y tienes "+ age +" años.")
